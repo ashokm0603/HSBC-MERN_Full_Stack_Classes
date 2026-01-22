51ebf3a0f316d8bb4bf970dc869fbd01d3cf9c68
ASHOKA M <ashokashokm2001@gmail.com>
2026-01-22 21:12:02 +0530

    day-7 class code

---
+++ Day-7 Class/DataTypes.js
@@ -0,0 +1,146 @@
+// a=10;
+// b="Javascript";
+// c=true;
+d=null;
+s=Symbol(522)
+
+f=54875623687625428567827265n
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+console.log(typeof d);
+console.log(typeof f);
+console.log(typeof s);
+
+
+arr=[10,20,30,4,5,60,]
+
+obj={
+    name:'sai',
+    role:"developer"
+}
+console.log( arr);
+console.log( obj);
+
+
+
+
+
+
+    let num;//variable Declaration
+    num="Javascript is a script language" //variable initialization
+    num=50;
+    console.log(num);//variable utilization 
+
+
+    let num1=50;
+    num1=60;
+
+    num1=90;
+    console.log(num1);
+    
+     
+
+
+    let age=8;
+    
+    let result =(age>=18)?"major":"miner"
+    console.log(result);
+    
+
+
+/*
+
+   // can not host let variables
+
+    console.log(var1);
+    let var1=50;
+
+    */
+
+
+   var name1="Bhavani"
+    var name1="Durga"
+
+
+
+    // console.log(salary);
+    
+    // let salary;
+
+
+    let role="developer"
+    let role2="full stack developer"
+   console.log(name1);
+   
+
+//    var fun="hello";
+//    fun=92222;
+//    fun="java"
+
+//    console.log(fun);
+
+//    var fun="python"
+//    console.log(fun);
+   
+//    console.log(  fun3);
+//    var fun3;
+   
+
+
+    const aadharNumber="8155 3355 2654"
+        
+    console.log(aadharNumber);
+
+
+    // const age; not possible
+    // age=50;
+
+
+    let address="Bangalore"
+
+    function addition(){
+
+    }
+    
+
+
+
+    let arr1=new Array("hello",20,30,4,0,50,);
+
+
+    console.log(typeof arr1);
+    
+    class Demo{
+
+    }
+
+
+    let  d1=new Demo();
+
+console.log(typeof d1);
+
+
+let collegeName1="RVIT"
+let collegeName2='St.Marys Engineering College  ${10+20}'
+
+let collegeName3=`Chaitanya 
+                Engineering 
+                                College  ${ 10+20}`
+
+
+console.log(collegeName1 , collegeName2);
+
+console.log(collegeName3);
+
+
+
+let a=10;
+let b=20;
+let c=`result addition of a and b : ${a+b}`
+console.log(c);
+
+
+
+
